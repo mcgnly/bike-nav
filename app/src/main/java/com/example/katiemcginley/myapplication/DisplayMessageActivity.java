d8d9634abc112ff10c85d9dbea0d191e2e97062b
Katie McGinley <katie@mcgnly.com>
2016-04-09 20:54:55 +0200

    add text view from the intent, displaying the message in a relative layout, no clue what the R is

--- app/src/main/java/com/example/katiemcginley/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/katiemcginley/myapplication/DisplayMessageActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -26,7 +28,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
-Intent intent = getIntent();
-    String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
 }
